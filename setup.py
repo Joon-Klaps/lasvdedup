cbddc2bf1c095045e5ea3d5d4edfe9fde890e92d
Joon-Klaps <joon.klaps@kuleuven.be>
2025-03-11 15:45:27 +0100

    setting max python version to 3.12.x because of new python API in rust

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
+    python_requires='>=3.9, <3.13',
     install_requires=[
         "biopython",
         "numpy",
