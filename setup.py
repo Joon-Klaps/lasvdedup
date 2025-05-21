48ced8fab12cda7ab47ce057683c108480f93c1d
Joon-Klaps <joon.klaps@kuleuven.be>
2025-05-21 12:04:55 +0200

    patch version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lasvdedup",
-    version="0.1.4",
+    version="0.1.5",
     packages=find_packages(),
     include_package_data=True,
     python_requires='>=3.9, <3.13',
